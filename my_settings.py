32227739aeb9c9365b643cc6da606bed80c0ea48
장진욱 <wfo3977@gmail.com>
2021-01-11 18:09:33 +0900

    First Commit

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": 'database 명',
+        "USER": "root",
+        "PASSWORD": 'DB 비밀번호',
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
+    }
+}
+
+SECRET = '시크릿키'
