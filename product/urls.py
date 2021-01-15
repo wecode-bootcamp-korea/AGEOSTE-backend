faa3ac135f772d81e67436f439370a26dd45668e
장진욱 <wfo3977@gmail.com>
2021-01-16 06:29:56 +0900

    [16기 장진욱] AGEOSTE-backend MODIFY ProductSearchView

--- product/urls.py
+++ product/urls.py
@@ -5,5 +5,5 @@ from .views      import ProductListView, ProductDetailView, ProductSearchView
 urlpatterns = [
     path('/<str:menu>/<str:sub_category>', ProductListView.as_view()),
     path('/<int:product_id>', ProductDetailView.as_view()),
-    path('', ProductSearchView.as_view()),
+    path('/search', ProductSearchView.as_view()),
 ]
