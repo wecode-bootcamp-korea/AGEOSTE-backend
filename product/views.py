d0dc8f9e32b1ac78c0673c23df21d99ce9f42d70
장진욱 <wfo3977@gmail.com>
2021-01-20 19:40:45 +0900

    [16기 장진욱] AGEOSTE-backend cartview & productdetailview update

--- product/views.py
+++ product/views.py
@@ -101,7 +101,7 @@ class ProductDetailView(View):
                 "code"             : product.code,
                 "description"      : product.description,
                 "price"            : product.price,
-                "sail_percent"     : product.discount_rate,
+                "discount_rate"     : product.discount_rate,
                 "review_score_avg" : product.score_avg,
 
                 "hashtags" : [{
