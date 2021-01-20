f00c6d443e8918b28b5685a09a245ac0d8fb4171
sue517 <csooa9305@gmail.com>
2021-01-20 15:14:39 +0900

    [16기 최수아] AGEOSTE-backend Add : PaymentView

--- order/urls.py
+++ order/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 
+from .views import PaymentView
+
 urlpatterns = [
+    path('/payment', PaymentView.as_view())
 ]
--- order/views.py
+++ order/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views import View
+from django.http  import JsonResponse
+
+from .models     import Cart
+from user.utils  import check_user
+from user.models import User, UserCoupon, Coupon
+
+
+class PaymentView(View):
+    @check_user
+    def patch(self, request):
+        data          = json.loads(request.body)
+        user          = request.user
+        user_address  = user.address
+        order_address = data.get('address')
+
+        if user_address is None:
+            User.objects.filter(id=user.id).update(address=order_address)
+            return JsonResponse({"message": "SUCCESS"}, status=200)
+
+
+    @check_user
+    def get(self, request):
+        try:
+            user    = request.user
+            carts   = Cart.objects.filter(user=user).select_related('product', 'user', 'size', 'color')
+            coupons = UserCoupon.objects.filter(user=user).select_related('user', 'coupon')
+
+            carts_list = [{
+                "product"       : cart.product.name,
+                "discount_rate" : cart.product.discount_rate,
+                "size"          : cart.size.name,
+                "color"         : cart.color.name,
+                "thumbnail"     : cart.thumbnail.image_url,
+                "quantity"      : cart.quantity
+            } for cart in carts]
+
+            coupons_list = [{
+                "coupon"               : coupon.coupon.name,
+                "coupon_discount_rate" : coupon.coupon.discount_rate
+            } for coupon in coupons]
+
+            membership = {
+                "grade"         : user.membership.grade,
+                "discount_rate" : user.membership.discount_rate
+            }
+
+            return JsonResponse({"carts_list" : carts_list, "coupons_list" : coupons_list, "membership" : membership}, status=200)
+
+        except CartDoesNotExist:
+            return JsonResponse({"error": "INVALID_CART"}, status=400)
+
+        except KeyError:
+            return JsonResponse({"error": "KEY_ERROR"}, status=400)
